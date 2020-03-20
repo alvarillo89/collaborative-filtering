e47bd57e1eca3347e8e5a235d8edcba75ffaf738
alvarillo89 <alvaro89@correo.ugr.es>
2020-03-20 16:31:39 +0100

    Neighborhood done

--- collfilter.py
+++ collfilter.py
@@ -3,24 +3,6 @@
 import pandas as pd
 
 
-def load_files(ratings, items):
-    """ This is just a example load function, made for the MovieLens
-    collection, you can write your own for your own collection 
-    """
-
-    user_names = ("user_id", "item_id", "rating", "timestamp")
-    items_names = ("movie_id","movie_title","release_date","video_release_date",
-        "IMDb_URL","unknown","Action","Adventure","Animation",
-        "Children's","Comedy","Crime","Documentary","Drama","Fantasy",
-        "Film-Noir","Horror","Musical","Mystery","Romance","Sci-Fi",
-        "Thriller","War","Western")
-
-    user = pd.read_csv(ratings, sep='\t', names=user_names, encoding="ISO-8859-1")
-    items = pd.read_csv(items, sep='|', names=items_names, encoding="ISO-8859-1")
-
-    return user, items
-
-
 class CollaborativeFilter:
     """ This class includes methods for building a neighborhood 
     and making recommendations to users based on collaborative 
@@ -29,17 +11,54 @@ class CollaborativeFilter:
 
     def __init__(self, user_ratings, items_info):
         """ Build an object of the class CollaborativeFilter:
-        user_ratings -- Pandas dataframe containing the rating that the users 
-            made of the items
-        items_info -- Pandas dataframe containing the items info. Each rating must
-            be related with some item of this file by its ID.
+        user_ratings -- Pandas dataframe containing the followings columns: 
+            user_id, item_id, rating
+        items_info -- Pandas dataframe containing at least one column named
+            item_id and all the other information that you want to show
+            about your items
         """
         if not (isinstance(user_ratings, pd.DataFrame) and isinstance(items_info, pd.DataFrame)):
-            raise AttributeError("user_ratings and items_info must be Pandas DataFrames")
+            raise AttributeError("user_ratings and items_info must be a Pandas DataFrames")
         
-        self.user = user_ratings
+        # Store items and user info
         self.items = items_info
-       
-    
-    def build_neighborhood(self, active_user_ratings):
-        pass
+        self.users = user_ratings
+        
+
+    def build_neighborhood(self, active_user_ratings, k):
+        """ Build the neighborhood from another user ratings:
+        active_user_ratings -- Pandas dataframe that contains the active user ratings.
+            This dataframe must contain the following columns: user_id, item_id, rating.
+        k -- size of the neighborhood. Return the k users more similars to active user 
+        """
+        if not isinstance(active_user_ratings, pd.DataFrame):
+            raise AttributeError("active_user_ratings must be a Pandas DataFrame")
+        
+        # Init output dataframe:
+        df = pd.DataFrame(columns=['user_id', 'Correlation'])        
+
+        # Get active user id:
+        active_id = active_user_ratings.iloc[0]['user_id']
+        # Add active user ratings to the existing ratings:
+        self.users = self.users.append(active_user_ratings, ignore_index=True)
+        
+        # Build the item-user table (invested for efficiency):
+        self.table = pd.pivot_table(self.users, values='rating', index="item_id", 
+            columns="user_id", fill_value=0)
+
+        # Compute correlations:
+        for user in self.table:
+            if user != active_id:
+                # Get a tmp dataframe with common items rated:
+                tmp = self.table[[user, active_id]]
+                tmp = tmp[(tmp.T != 0).all()]
+                if tmp.shape[0] < 4: continue   # Avoid NaN errors
+                corr = pd.DataFrame(tmp.corrwith(tmp[active_id], method='pearson'), 
+                    columns=['Correlation'])
+                df = df.append({"user_id": corr.index[0], 
+                    "Correlation": corr.iloc[0]['Correlation']}, ignore_index=True)
+
+        df = df.astype({"user_id": "int64"}, copy=False)
+        df = df.sort_values(by='Correlation', ascending=False).head(k)
+        df = df.reset_index(drop=True)
+        return df
--- main.py
+++ main.py
@@ -2,36 +2,68 @@
 
 import random
 import pandas as pd
-from collfilter import CollaborativeFilter, load_files
+from collfilter import CollaborativeFilter
 
 
-def get_user_ratings(films, n_films=10):
+def load_files(ratings, items):
+    """ This is just a example load function, made for the MovieLens
+    collection, you can write your own for your own collection 
+    """
+
+    user_names = ("user_id", "item_id", "rating", "timestamp")
+    items_names = ("item_id","movie_title","release_date","video_release_date",
+        "IMDb_URL","unknown","Action","Adventure","Animation",
+        "Children's","Comedy","Crime","Documentary","Drama","Fantasy",
+        "Film-Noir","Horror","Musical","Mystery","Romance","Sci-Fi",
+        "Thriller","War","Western")
+
+    user = pd.read_csv(ratings, sep='\t', names=user_names, encoding="ISO-8859-1")
+    items = pd.read_csv(items, sep='|', names=items_names, encoding="ISO-8859-1")
+
+    # Get just important columns and drop the others:
+    user = user[["user_id", "item_id", "rating"]]
+    items = items[["item_id", "movie_title"]]
+
+    return user, items
+
+
+def get_user_ratings(films, user_id, n_films, rand=False):
     """ Ask active user to rate, from 1 to 5, `n_films` random films from
-    `films` DataFrame. Returns a new dataframe with film id and user rating
+    `films` DataFrame. Returns a new dataframe with the same format as
+        user_ratings
+    user_id -- the id that will be assigned to the active user. It must
+        be different from the others present in your data
+    rand -- If True, active user rates will be generated randomly 
     """
     n = films.shape[0]
-    df = pd.DataFrame(columns=("item_id", "rating"))
+    df = pd.DataFrame(columns=("user_id", "item_id", "rating"))
     for _ in range(n_films):
         index = random.randint(0, n-1)
         film_title = films.iloc[index]["movie_title"]
-        film_id = films.iloc[index]["movie_id"]
-        rate = input("What do you think about %s? [1-5]: " % film_title)
-        
-        while type(rate) is str:
-            try:
-                rate = int(rate)
-                if not (1 <= rate and rate <= 5): raise Exception
-            except Exception:
-                rate = input("Oops! Something went wrong, ensure that your" + 
-                    "rate is between 1 and 5: ")
+        film_id = films.iloc[index]["item_id"]
 
-        df = df.append({'item_id': film_id, "rating": rate}, ignore_index=True)     
-    
-    return df
+        if not rand:
+            rate = input('What do you think about "%s"? [1-5]: ' % film_title)
+            while type(rate) is str:
+                try:
+                    rate = int(rate)
+                    if not (1 <= rate and rate <= 5): raise Exception
+                except Exception:
+                    rate = input("Oops! Something went wrong, ensure that your" + 
+                        "rate is between 1 and 5: ")
+        else:
+            rate = random.randint(1,5)
+
+        df = df.append({'user_id': user_id, 'item_id': film_id, "rating": rate}, 
+            ignore_index=True)     
+
+    return df.astype('int64')
 
 
 if __name__ == "__main__":
     # Load data:
+    random.seed(89)
     user, items = load_files(ratings='./ml-data/u.data', items='./ml-data/u.item')
     cf = CollaborativeFilter(user_ratings=user, items_info=items)
-    active_user_ratings = get_user_ratings(items)
+    active_user_ratings = get_user_ratings(films=items, user_id=944, n_films=20, rand=True)
+    neighbor = cf.build_neighborhood(active_user_ratings=active_user_ratings, k=10)
